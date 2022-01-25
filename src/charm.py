6214d826692af31b1a19fb44e7b6f8dccf678546
phvalguima <phvalguima@gmail.com>
2022-01-25 23:43:58 +0100

    Add prometheus and grafana dashboards, update the charm

--- src/charm.py
+++ src/charm.py
@@ -2,6 +2,8 @@
 
 import logging
 
+from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
+from charms.grafana_k8s.v0.grafana_dashboard import GrafanaDashboardProvider
 from jinja2 import Environment, FileSystemLoader
 from ops.charm import CharmBase
 from ops.main import main
@@ -32,6 +34,28 @@ class Operator(CharmBase):
                 self.on[rel].relation_changed,
                 self.set_pod_spec,
             )
+        self.prometheus_provider = MetricsEndpointProvider(
+            charm=self,
+            relation_name="monitoring",
+            jobs=[
+                {
+                    "job_name": "seldon_core_metrics",
+                    "scrape_interval": "30s",
+                    "metrics_path": self.config["executor-server-metrics-port-name"],
+                    "static_configs": [
+                        {"targets": ["*:{}".format(self.config["metrics-port"])]}
+                    ],
+                }
+            ],
+        )
+        self.dashboard_provider = GrafanaDashboardProvider(self)
+        monitoring_events = [
+            self.on["monitoring"].relation_changed,
+            self.on["monitoring"].relation_broken,
+            self.on["monitoring"].relation_departed,
+        ]
+        for e in monitoring_events:
+            self.framework.observe(e, self.set_pod_spec)
 
     def set_pod_spec(self, event):
         if not self.model.unit.is_leader():
--- tests/unit/test_operator.py
+++ tests/unit/test_operator.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 from ops.model import ActiveStatus, BlockedStatus
 from ops.testing import Harness
 
@@ -36,3 +37,32 @@ def test_no_relation(harness):
     harness.begin_with_initial_hooks()
 
     assert harness.charm.model.unit.status == ActiveStatus("")
+
+
+def test_prometheus_data_set(harness, mocker):
+    harness.set_leader(True)
+    harness.set_model_name("kubeflow")
+    harness.begin()
+
+    mock_net_get = mocker.patch("ops.testing._TestingModelBackend.network_get")
+    mocker.patch("ops.testing._TestingPebbleClient.list_files")
+
+    bind_address = "1.1.1.1"
+    fake_network = {
+        "bind-addresses": [
+            {
+                "interface-name": "eth0",
+                "addresses": [
+                    {"hostname": "cassandra-tester-0", "value": bind_address}
+                ],
+            }
+        ]
+    }
+    mock_net_get.return_value = fake_network
+    rel_id = harness.add_relation("monitoring", "otherapp")
+    harness.add_relation_unit(rel_id, "otherapp/0")
+    harness.update_relation_data(rel_id, "otherapp", {})
+
+    assert json.loads(
+        harness.get_relation_data(rel_id, harness.model.app.name)["scrape_jobs"]
+    )[0]["static_configs"][0]["targets"] == ["*:8080"]
