c6020d0b2cfc93de35d27acb3573c1c3a147bc44
Kenneth Koski <kenneth.koski@canonical.com>
2022-01-26 10:28:04 -0600

    Add basic deployment testing
    Extends integration test to deploy mock classifier, wait for it to come
    up, and then test that it gets a response.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,7 +1,12 @@
+import time
 from pathlib import Path
 
 import pytest
+import requests
 import yaml
+from lightkube import Client
+from lightkube.generic_resource import create_namespaced_resource
+from lightkube.resources.core_v1 import Service
 from pytest_operator.plugin import OpsTest
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
@@ -18,6 +23,16 @@ async def test_build_and_deploy(ops_test: OpsTest):
     image_path = METADATA["resources"]["oci-image"]["upstream-source"]
     resources = {"oci-image": image_path}
 
+    await ops_test.run(
+        "kubectl",
+        "label",
+        "namespace",
+        "default",
+        "serving.kubeflow.org/inferenceservice=enabled",
+        "--overwrite",
+        check=True,
+    )
+
     await ops_test.model.deploy(
         charm_under_test, resources=resources, application_name=APP_NAME, trust=True
     )
@@ -25,3 +40,49 @@ async def test_build_and_deploy(ops_test: OpsTest):
         apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=60 * 10
     )
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+
+    SeldonDeployment = create_namespaced_resource(
+        group="machinelearning.seldon.io",
+        version="v1",
+        kind="seldondeployment",
+        plural="seldondeployments",
+        verbs=None,
+    )
+
+    client = Client()
+
+    with open("examples/serve-simple-v1.yaml") as f:
+        sdep = SeldonDeployment(yaml.safe_load(f.read()))
+        client.create(sdep)
+
+    for i in range(30):
+        dep = client.get(SeldonDeployment, "seldon-model")
+
+        if dep.get("status", {}).get("state") == "Available":
+            break
+
+        time.sleep(5)
+    else:
+        pytest.fail("Waited too long for seldondeployment/seldon-model!")
+
+    service_name = "seldon-model-example-classifier"
+    service = client.get(Service, name=service_name)
+    service_ip = service.spec.clusterIP
+    service_port = next(p for p in service.spec.ports if p.name == "http").port
+
+    response = requests.post(
+        f"http://{service_ip}:{service_port}/predict",
+        json={
+            "data": {
+                "names": ["a", "b"],
+                "tensor": {"shape": [2, 2], "values": [0, 0, 1, 1]},
+            }
+        },
+    )
+    response.raise_for_status()
+
+    response = response.json()
+
+    assert response["data"]["names"] == ["proba"]
+    assert response["data"]["tensor"]["shape"] == [2, 1]
+    assert response["meta"] == {}
