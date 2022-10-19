1248c7b5c18fb2bf9d0070ec29c4688ed71b036f
Ivan Chvets <ivan.chvets@canonical.com>
2022-10-19 13:21:54 -0400

    Feature: Seldon Core charm re-write using sidecar https://github.com/canonical/seldon-core-operator/issues/42
    Summary of changes:
    - Bumped upload-charm.
    - Addressed code review comments. Added longer idle parameter to make sure all resources are deployed.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -36,8 +36,9 @@ async def test_build_and_deploy(ops_test: OpsTest):
         charm_under_test, resources=resources, application_name=APP_NAME, trust=True
     )
 
+    # NOTE: idle_period is used to ensure all resources are deployed
     await ops_test.model.wait_for_idle(
-        apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=60 * 10
+        apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=60 * 10, idle_period=30
     )
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
