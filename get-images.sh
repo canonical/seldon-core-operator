a31412b8a683d9a2682ede7e7f2c2f608b1bba3c
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-10 07:41:53 -0400

    fix: added description and comments

--- get-images.sh
+++ get-images.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# This script returns list of container images that are managed by this charm and/or its workload
+#
+# static list
 STATIC_IMAGE_LIST=(
 docker.io/seldonio/engine:1.12.0
 docker.io/charmedkubeflow/sklearnserver_v1.16.0_20.04_1_amd64:v1.16.0_20.04_1
@@ -10,6 +14,7 @@ nvcr.io/nvidia/tritonserver:21.08-py3
 seldonio/mlserver:1.2.0-huggingface
 seldonio/mlserver:1.2.0-slim
 )
+# dynamic list
 IMAGE_LIST=()
 IMAGE_LIST+=$(yq '.options | ."executor-container-image-and-version" | .default' config.yaml)
 
