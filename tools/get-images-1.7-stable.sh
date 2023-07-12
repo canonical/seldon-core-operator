07d9e5135444095776d7c248e0edb65e9596cb66
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-12 13:22:12 -0400

    fix: typo

--- tools/get-images-1.7-stable.sh
+++ tools/get-images-1.7-stable.sh
@@ -11,7 +11,7 @@ docker.io/seldonio/engine:1.12.0
 git checkout origin/track/1.15
 IMAGE_LIST=()
 IMAGE_LIST+=($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
-IMAGE_LIST=($(yq '.options.executor-container-image-and-version.default' config.yaml))
+IMAGE_LIST+=($(yq '.options.executor-container-image-and-version.default' config.yaml))
 IMAGE_LIST+=($(yq e ".data.predictor_servers" src/templates/configmap.yaml.j2 | jq -r '.[]' | jq -r 'select((.protocols.v2)) | "\(.protocols.v2.image):\(.protocols.v2.defaultImageVersion)"'))
 IMAGE_LIST+=($(yq e ".data.predictor_servers" src/templates/configmap.yaml.j2 | jq -r '.[]' | jq -r 'select((.protocols.seldon)) | "\(.protocols.seldon.image):\(.protocols.seldon.defaultImageVersion)"'))
 IMAGE_LIST+=($(yq '.options | ."executor-container-image-and-version" | .default' config.yaml))
