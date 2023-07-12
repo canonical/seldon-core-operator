55fdd578c19a537a36fe29fb1555dcbb9e9a520d
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-12 13:20:14 -0400

    fix: added dynamic images
    Summary of changes:
    - Added dynamic extraction of metadata images.
    - Added executor retrieval.

--- tools/get-images-1.7-stable.sh
+++ tools/get-images-1.7-stable.sh
@@ -6,11 +6,12 @@
 STATIC_IMAGE_LIST=(
 # from seldon-core
 docker.io/seldonio/engine:1.12.0
-seldonio/seldon-core-executor:1.13.1
 )
 # dynamic list
 git checkout origin/track/1.15
 IMAGE_LIST=()
+IMAGE_LIST+=($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
+IMAGE_LIST=($(yq '.options.executor-container-image-and-version.default' config.yaml))
 IMAGE_LIST+=($(yq e ".data.predictor_servers" src/templates/configmap.yaml.j2 | jq -r '.[]' | jq -r 'select((.protocols.v2)) | "\(.protocols.v2.image):\(.protocols.v2.defaultImageVersion)"'))
 IMAGE_LIST+=($(yq e ".data.predictor_servers" src/templates/configmap.yaml.j2 | jq -r '.[]' | jq -r 'select((.protocols.seldon)) | "\(.protocols.seldon.image):\(.protocols.seldon.defaultImageVersion)"'))
 IMAGE_LIST+=($(yq '.options | ."executor-container-image-and-version" | .default' config.yaml))
