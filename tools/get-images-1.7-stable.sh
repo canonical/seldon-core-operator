3e43aee0643386e5f0ec1a77aabd2f02c5ecbf2a
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-12 09:19:26 -0400

    feat: update script to use 1.7 release branch

--- tools/get-images-1.7-stable.sh
+++ tools/get-images-1.7-stable.sh
@@ -4,28 +4,20 @@
 #
 # static list
 STATIC_IMAGE_LIST=(
-# from configmap
-tensorflow/serving:2.1.0
-seldonio/tfserving-proxy:1.15.0
-docker.io/charmedkubeflow/sklearnserver_v1.16.0_20.04_1_amd64:v1.16.0_20.04_1
-docker.io/charmedkubeflow/mlserver-sklearn_1.2.0_22.04_1_amd64:1.2.0_22.04_1
-seldonio/xgboostserver:1.15.0
-docker.io/charmedkubeflow/mlserver-xgboost_1.2.0_22.04_1_amd64:1.2.0_22.04_1
-seldonio/mlflowserver:1.15.0
-docker.io/charmedkubeflow/mlserver-mlflow_1.2.0_22.04_1:1.2.0_22.04_1
-nvcr.io/nvidia/tritonserver:21.08-py3
-docker.io/charmedkubeflow/mlserver-huggingface_1.2.4_22.04_1_amd64:1.2.4_22.04_1
-seldonio/mlserver:1.2.0-slim
-seldonio/rclone-storage-initializer:1.14.1
-seldonio/alibiexplainer:1.15.0
-seldonio/mlserver:1.2.0-alibi-explain
 # from seldon-core
 docker.io/seldonio/engine:1.12.0
 seldonio/seldon-core-executor:1.13.1
 )
 # dynamic list
+git checkout origin/track/1.15
 IMAGE_LIST=()
-IMAGE_LIST+=$(yq '.options | ."executor-container-image-and-version" | .default' config.yaml)
+IMAGE_LIST+=($(yq e ".data.predictor_servers" src/templates/configmap.yaml.j2 | jq -r '.[]' | jq -r 'select((.protocols.v2)) | "\(.protocols.v2.image):\(.protocols.v2.defaultImageVersion)"'))
+IMAGE_LIST+=($(yq e ".data.predictor_servers" src/templates/configmap.yaml.j2 | jq -r '.[]' | jq -r 'select((.protocols.seldon)) | "\(.protocols.seldon.image):\(.protocols.seldon.defaultImageVersion)"'))
+IMAGE_LIST+=($(yq '.options | ."executor-container-image-and-version" | .default' config.yaml))
+IMAGE_LIST+=($(yq e ".data.storageInitializer" src/templates/configmap.yaml.j2 | jq -r 'select((.image)) | "\(.image)"'))
+IMAGE_LIST+=($(yq e ".data.explainer" src/templates/configmap.yaml.j2 | jq -r 'select((.image)) | "\(.image)"'))
+IMAGE_LIST+=($(yq e ".data.explainer" src/templates/configmap.yaml.j2 | jq -r 'select((.image_v2)) | "\(.image_v2)"'))
+
 
 printf "%s\n" "${STATIC_IMAGE_LIST[@]}"
 printf "%s\n" "${IMAGE_LIST[@]}"
